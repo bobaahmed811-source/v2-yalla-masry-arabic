b3e7add8d2d5f76738c573312e4c8bc11047ec07
boba Ahmed <bobaahmed811@gmail.com>
2025-12-11 18:19:10 +0000

    اوك

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  projectId: "studio-3727091711-291eb",
-  appId: "1:344732522982:web:75c22519f6579e4cbf5e27",
-  apiKey: "AIzaSyBYPnhD2bKTJfTfne0GM9dSQNnxNDLbCmc",
-  authDomain: "studio-3727091711-291eb.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "344732522982",
+  "projectId": "studio-3727091711-291eb",
+  "appId": "1:344732522982:web:75c22519f6579e4cbf5e27",
+  "apiKey": "AIzaSyBYPnhD2bKTJfTfne0GM9dSQNnxNDLbCmc",
+  "authDomain": "studio-3727091711-291eb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "344732522982"
 };
+
