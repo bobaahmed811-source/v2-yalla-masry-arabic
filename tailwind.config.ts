8f597ce4f9b524e59ffc16e3adc585e95def9e85
boba Ahmed <bobaahmed811@gmail.com>
2025-12-23 11:13:19 +0000

    لم يحدث تغير اريد الترتيب من داخل المستودع هل يمكنك ا\ا ارسلت اسمه لك

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -6,6 +7,9 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './firebase/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
